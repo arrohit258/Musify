6715cf61b3e2eb2fcbbc56e299eb2ceacd5dfcb2
Rohit Arora <63255932+arrohit258@users.noreply.github.com>
2021-06-13 01:44:35 +0530

    Delete src/AuthParams directory

--- src/AuthParams/authKey.js
+++
@@ -1,2 +0,0 @@
-const client_id='cb70eaed88a34c4c98c8f940a76d7844'
-module.exports={client_id}
--- src/AuthParams/authSecret.js
+++
@@ -1,2 +0,0 @@
-const client_secret="6cdbd869418d40e0a2be8de8a61badd5"
-module.exports={client_secret}
